42107808c2435fe4f2fd245e74114e49a50dced8
Ziad Feghali the king <ziadfeghalitheking@ziads-mbp.home>
2021-07-14 15:59:34 -0400

    added jquery function to change the first 3 images on scroll, concept still buggy but acceptable

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 ﻿# toomsauce_heroPage
 Live version available on https://toomsauce.com/toomHeroes
+
+TO DO LIST:
+7-14-21
+Must have:
+1- add background images. 
+2- change backgound images based on scolling value or specific divs in the viewport
+Nice to have:
+3- create a nice fade effect for the images
+4- change the colors of the middle line, cercles around heo icons and titles based on the product that is on screen
+Could have:
+5- Contact and social icons on medium and large screens
+
--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,30 @@ function toggleMenu() {
 }
 let x = document.getElementById("hambrgrBtn");
 x.onclick = toggleMenu;
+
+var img1 = "url(https://toomsauce.com/wp-content/uploads/2020/08/original-1494-600x600.png)"
+  , img2 = "url(https://toomsauce.com/wp-content/uploads/2020/08/square_Toom.png)"
+  , img3 = "url(https://toomsauce.com/wp-content/uploads/2020/08/original-1497-600x600.png)"
+
+$(document).ready(function() {
+  $(".img1").css("background-image", img1)
+  $(".img2").css("background-image", img2)
+  $(".img3").css("background-image", img3)
+
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop()
+
+    if (scrollTop < 500) {
+      $(".img1").css('opacity', 100/scrollTop)
+      console.log(scrollTop)
+    }else if (scrollTop >500 && scrollTop < 1000) {
+        $('.img2').css('opacity', 350/(scrollTop-500))
+        $(".img1").css('opacity', 0)
+        console.log(scrollTop)
+    }else if (scrollTop >1300 && scrollTop < 2000) {
+        $('.img3').css('opacity', 450/(scrollTop-1300))
+        $(".img2").css('opacity', 0)
+        console.log(scrollTop)
+    } 
+  }) //End of scroll function
+})
